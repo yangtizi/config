822901930c9a85300b41e00ba6c743d149126d55
warrially <warrially@139.com>
2021-08-18 16:58:49 +0800

    修复原先只能处理 \t 的BUG

--- jsonconfig/jsonconfig.go
+++ jsonconfig/jsonconfig.go
@@ -4,21 +4,29 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"errors"
 	"os"
 	"strings"
 
-	log "github.com/yangtizi/log/zaplog"
+	"github.com/yangtizi/go/ioutils"
+	"github.com/yangtizi/log/zaplog"
 )
 
 //JSONParsing 解析json文件, 文件名, 解析的JSON结构体
-func JSONParsing(strPath string, v interface{}) {
+func JSONParsing(strPath string, v interface{}) error {
+	if !ioutils.File.Exists(strPath) {
+		zaplog.Errorf("yaml需要加载配置，但是找不到文件 %s", strPath)
+		return errors.New("找不到文件 " + strPath)
+	}
+
 	//创建一个新的buff
 	buf := new(bytes.Buffer)
 	//打开文件
 	f, err := os.Open(strPath)
-	// f,err :=  io.ReadFile(strPath)
+
 	if err != nil {
-		log.Errorf("%v", err)
+		zaplog.Errorf("打开文件 [%s] 错误 [%v]", strPath, err)
+		return err
 	}
 	defer f.Close()
 
@@ -32,11 +40,19 @@ func JSONParsing(strPath string, v interface{}) {
 			}
 			break
 		}
-		if !strings.HasPrefix(strings.TrimLeft(string(line), "\t"), "//") {
+
+		strLine := strings.TrimLeft(string(line), "\t")  // 去掉TAB
+		strLine = strings.TrimLeft(string(strLine), " ") // 去掉空格
+
+		if !strings.HasPrefix(strLine, "//") {
 			buf.Write(line)
 		}
 	}
 	//解析json
+	err = json.Unmarshal([]byte(buf.Bytes()), v)
+	if err != nil {
+		zaplog.Errorf("解包文件 [%s] 错误 [%v]", strPath, err)
+	}
 
-	json.Unmarshal([]byte(buf.Bytes()), v)
+	return err
 }
